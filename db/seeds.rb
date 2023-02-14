21bfc419bb18b97ea050297a2ba0a8516c2e86fb
Hassan Razak <hassanrazak1991@gmail.com>
2023-02-14 11:12:58 -0500

    added user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+rochelle = User.create!(first_name:"Rochelle", last_name:"Katzman", email: "rochelleK@gmail.com",password:'1234')
+veronique = User.create!(first_name:"Veronique", last_name:"Cadet", email: "vero-cadet@gmail.com",password:'1234')
+hassan = User.create!(first_name:"Hassan", last_name:"Razak", email: "hassan-razak@gmail.com",password:'1234')
