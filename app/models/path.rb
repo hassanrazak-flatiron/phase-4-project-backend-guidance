f7fccc4662c317beb81528cd4ac0cdc60c4dbee6
Hassan Razak <hassanrazak1991@gmail.com>
2023-02-15 12:43:17 -0500

    validations added for career, path, and user models

--- app/models/path.rb
+++ app/models/path.rb
@@ -2,9 +2,19 @@ class Path < ApplicationRecord
   belongs_to :user
   belongs_to :career
 
-  # validations
-  # user_id  presence true 
-  # career_id presence true
-  # career_id uniqueness: true - - implementation of scope. 
+  ##### validations #####
+  
+  ##### user_id #####  
+  validates :user_id, presence:true
+  
+  ##### career_id presence true ######
+  validates :career_id, presence:true
 
+  #### career_id  ######
+  # NEED TO REVISIT THIS, AND FIGURE OUT HOW TO 
+  # ASSIGN ONLY ONE CAREER PATH PER USER_ID. NO DUPLICATE CAREER_IDs PER USER.
+  validates :career_id, uniqueness:true 
+
+
+  
 end
