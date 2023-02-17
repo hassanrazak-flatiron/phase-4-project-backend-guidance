e24f02d1009eedcfae5e4a8120a862bd0a9d1426
Veronique Cadet <cadetvero@gmail.com>
2023-02-17 14:26:07 -0500

    new seeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,12 @@ class UsersController < ApplicationController
         render json: User.find(session[:user_id])
     end
 
+    def update
+        user = User.find(session[:user_id])
+        user.update!(user_params)
+        render json: user, status: :accepted
+    end
+
     private
 
     def user_params
